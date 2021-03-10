7431c1ac375d8c4319d8cc2f25ab531d002ed12f
Baxter Alexander <baxter.alexander@student.onslow.school.nz>
2021-03-11 08:44:25 +1300

    Copied work from home into the shared project for ease of use. Made it up to poiint wher ticekts are made with random numbers

---
+++ Strike.java
@@ -0,0 +1,28 @@
+/**
+ * Write a description of class Spike here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Strike
+{
+    // Create main method of Spike game
+    public static void main(String[] args)
+    {
+        Ticket myTicket = new Ticket();
+        Ticket myLotto = new Ticket();
+        final int LIMIT = 6;
+        int[] ticketNum = myTicket.getTicket();
+            
+        int[] lottoNum = myLotto.getLotto();
+        for( int index = 0; index < LIMIT; index ++)
+           { 
+               System.out.println(ticketNum[index] + "  ");
+            }
+        for( int index = 0; index < LIMIT; index ++)
+           { 
+               System.out.println(lottoNum[index] + "  ");
+            }
+        
+    }
+}
